2f92853dd9d75caabba62316555da8af0f3251ad
mathvra <matheusvram@gmail.com>
2021-08-10 13:47:03 -0300

    componente e classe porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+import Porta from "../components/Porta";
 import Presente from "../components/Presente";
+import PortaModel from "../model/porta";
 
 export default function Home() {
+
+  const [p1, setP1] = useState(new PortaModel(1))
+
   return (
-    <div>
-      <Presente/>
+    <div style={{display: "flex"}}>
+      <Porta porta = {p1}/>
     </div>
   )
 }
