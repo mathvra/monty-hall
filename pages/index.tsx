f4160956d54d2fb927f98fead62c9626a3f9dc2c
mathvra <matheusvram@gmail.com>
2021-08-11 13:23:46 -0300

    eventos para a porta

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,11 @@ import Presente from "../components/Presente";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-
   const [p1, setP1] = useState(new PortaModel(1))
 
   return (
     <div style={{display: "flex"}}>
-      <Porta porta = {p1}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
     </div>
   )
 }
