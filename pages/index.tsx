0afe465873ed021871d0f458aa92af8176be84bf
mathvra <matheusvram@gmail.com>
2021-08-12 13:19:04 -0300

    array de porta e componente jogo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
 import PortaModel from "../model/porta";
+import { atualizarPortas, criarPortas } from "../functions/portas";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
-
   return (
     <div style={{display: "flex"}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+      <h1>Formulário de Início!!!</h1>
     </div>
   )
 }
---
+++ pages/jogo.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Porta from "../components/Porta";
+import { atualizarPortas, criarPortas } from "../functions/portas";
+import styles from "../styles/Jogo.module.css"
+import Link from "next/link"
+
+export default function jogo(){
+    const [portas, setPortas] = useState(criarPortas(5,2))
+
+    function renderizarPortas(){
+        return portas.map(porta => {
+        return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
+        })
+    }
+
+    return (
+        <div className={styles.jogo}>
+            <div className={styles.portas}>
+                {renderizarPortas()}
+            </div>
+            <div className={styles.botoes}>
+                <Link href="/">
+                    <button>Reiniciar Jogo</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
