9df7622088c3bdeaecfc2465dd2184e82e0e2060
mathvra <matheusvram@gmail.com>
2021-08-13 13:41:55 -0300

    usando router e estruturando formulario

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
-import Porta from "../components/Porta";
-import Presente from "../components/Presente";
-import PortaModel from "../model/porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
+import Cartao from "../components/Cartao";
+import styles from "../styles/Formulario.module.css"
+import Link from "next/link"
 
-export default function Home() {
+export default function Formulario() {
   return (
-    <div style={{display: "flex"}}>
-      <h1>Formulário de Início!!!</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   )
 }
